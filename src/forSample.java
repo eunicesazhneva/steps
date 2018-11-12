d61e11c4f336869d8ef14c705fc799834c396c12
eunicesazhneva <evnikasazhneva@gmail.com>
2018-11-12 14:41:34 +0200

    simple for + if

---
+++ src/forSample.java
@@ -0,0 +1,9 @@
+public class forSample {
+    public static void main (String args[]){
+        int x, y;
+        y = 2;
+        for (x = 0; x < 10; x++)
+
+          if (x%y == 0) System.out.println("x равен: " + x);
+    }
+}
