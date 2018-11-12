238d08652d43ed5bce15780ed9f9a61b420e1b43
eunicesazhneva <evnikasazhneva@gmail.com>
2018-11-12 14:23:38 +0200

    second step

--- firststep.java
+++
@@ -1,5 +0,0 @@
-class firststep {
-    public static void main (String args []){
-        System.out.println("First step in Java");
-    }
-}
---
+++ secondstep.java
@@ -0,0 +1,9 @@
+class secondstep {
+    public static void main (String args []){
+        int num;
+        num = 100;
+        System.out.println("num: " + num);
+        num = num*2;
+        System.out.println("num * 2: " + num);
+    }
+}
