6a02b6c5bfcc0babc105370b79129e175b621602
eunicesazhneva <evnikasazhneva@gmail.com>
2018-11-12 14:32:33 +0200

    simple if

--- secondstep.java
+++
@@ -1,9 +0,0 @@
-class secondstep {
-    public static void main (String args []){
-        int num;
-        num = 100;
-        System.out.println("num: " + num);
-        num = num*2;
-        System.out.println("num * 2: " + num);
-    }
-}
